67a5ca4470df0a37b042a8c6bd0976bc80e1d0fe
Avneesh Sarwate <avneeshsarwate@gmail.com>
2025-10-08 02:36:50 +0100

    more copy fixes

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,15 @@ import PianoRollDemo from './components/PianoRollDemo.vue'
 
 <template>
   <main class="page">
-    <h1>Piano Roll with AI Music Assistant</h1>
+    <h1>LLM MIDI Mangler</h1>
     <div class="description">
       <p>
-        This is an LLM powered midi mangling workbench. A central piano roll lets you manually write out midi, 
+        This is an LLM powered MIDI mangling workbench. A central piano roll lets you manually write out MIDI, 
         and it is played back using <a href="https://tonejs.github.io/" target="_blank" rel="noreferrer">Tone.js</a>. 
         A chatbot powered by Claude Sonnet can read and write from the piano roll with tool calls. Additionally, 
         there is a workbench at the bottom of the page that lets you define custom functions for transforming 
         the piano roll. The chatbot can write new functions into specific slots, or can even call your functions 
-        as tools (instead of generating raw MIDI as an LLM response).
+        as tools (instead of generating raw MIDI as an LLM response). You can also manually apply your transformation functions with the controls under the text editor.
         <br/><br/>
         The source code for this is on <a href="https://github.com/AvneeshSarwate/pianoRollChatbot" target="_blank" rel="noreferrer">GitHub</a>.
       </p>
